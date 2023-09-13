868eb9148ec53441236040875145659884158e93
phung10 <trieuphung.2006@gmail.com>
2023-09-13 20:54:23 +0700

    up

---
+++ src/_01_introduction_java/baitap/Hienthilochao.java
@@ -0,0 +1,13 @@
+package _01_introduction_java.baitap;
+
+import java.util.Scanner;
+
+public class Hienthilochao {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String  name = sc.nextLine();
+
+        System.out.println("Hello: " + name);
+    }
+}
