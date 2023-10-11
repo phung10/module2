ec5a32bd86c587e07d3bb2a7387a135cc8d139e6
phung10 <trieuphung.2006@gmail.com>
2023-10-11 18:56:15 +0700

    up

---
+++ src/_06_ke_thua/bai4/Shape.java
@@ -0,0 +1,24 @@
+package _06_ke_thua.bai4;
+
+public class Shape {
+    private String color = "Green";
+    public Shape() {}
+    public Shape(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "color='" + color + '\'' +
+                '}';
+    }
+}
---
+++ src/_06_ke_thua/bai4/Triangle.java
@@ -0,0 +1,74 @@
+package _06_ke_thua.bai4;
+
+
+public class Triangle extends Shape {
+
+    private double side1;
+    private double side2;
+    private double side3;
+
+    public Triangle() {
+        this.side1 = 1.0;
+        this.side2 = 1.0;
+        this.side3 = 1.0;
+    }
+
+    public Triangle(double side1, double side2, double side3, String color) {
+        super(color);
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    public double getSide1() {
+        return side1;
+    }
+
+    public void setSide1(double side1) {
+        this.side1 = side1;
+    }
+
+    public double getSide2() {
+        return side2;
+    }
+
+    public void setSide2(double side2) {
+        this.side2 = side2;
+    }
+
+    public double getSide3() {
+        return side3;
+    }
+
+    public void setSide3(double side3) {
+        this.side3 = side3;
+    }
+
+
+    public double getArea() {
+        double s = (side1 + side2 + side3) / 2;
+        return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+    }
+
+
+    public double getPerimeter() {
+        return side1 + side2 + side3;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "side1=" + side1 +
+                        ", side2=" + side2 +
+                        ", side3=" + side3 ;
+    }
+
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle(6.0, 11.0, 9.0,"Red");
+        System.out.println("Diện tích tam giác: " + triangle.getArea());
+        System.out.println("Chu vi tam giác: " + triangle.getPerimeter());
+        System.out.println("Thông tin tam giác: " + triangle);
+        System.out.println("Màu: "+triangle.getColor());
+    }
+
+}
